35e3a18ef1f415cbc269a9aea1ffdb6e1535baf1
yusprasetio21 <akbaryusprasetio.com>
2026-04-23 08:57:37 +0700

    update config supabase

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://gbwiftzlqmnmqpnovzoo.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdid2lmdHpscW1ubXFwbm92em9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MDc5ODgsImV4cCI6MjA5MTk4Mzk4OH0.eiF-8nkfq3XNV0MmuGV_6Lys6YAFXxcPHM32u29L4ow",
+  },
 };
 
 export default nextConfig;
